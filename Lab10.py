b991b5bc2572677cca408d1aa77f2952312f717e
reyn0149 <reyn0149@algonquinlive.com>
2019-11-17 17:42:56 -0500

    Added the ability to open the web browser successfully

--- Lab10.py
+++ Lab10.py
@@ -1,3 +1,19 @@
-userInput = int(input("Please make a selection between 0 and 24!"))
+import sqlite3, base64, webbrowser
+userInput = 0
+#Initializing the database
+connect = sqlite3.connect('week10.db')
+connect.row_factory = lambda cursor, row: row[0]
+cursor = connect.cursor()
 
-print("user input")
+#If the user inputs an invalid number, the program asks for input again
+while userInput < 1 or userInput > 24:
+    userInput = input("Please make a selection between 1 and 24! ")
+    #Exiting the program if the user inputs q
+    if userInput == "q":
+        exit()
+    userInput = int(userInput)
+    #Opens the web browser to the google maps address selected
+    if userInput >= 1 and userInput <= 24:
+        userTuple = (userInput, )
+        cursor.execute('SELECT link FROM  Lab10 WHERE id=?', userTuple)
+        webbrowser.open(base64.b64decode(cursor.fetchone()))
